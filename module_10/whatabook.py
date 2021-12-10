439d66198caac2fc6ed7a824a14735dad512b402
ryan-kite <ryankiter@gmail.com>
2021-12-10 00:41:15 -0800

    added more code and logic

--- module_10/whatabook.py
+++ module_10/whatabook.py
@@ -4,10 +4,12 @@ CSD 310
 Module 10
 Assignement WhatABook 
 '''
+import time
+import sys
 import mysql.connector
 from mysql.connector import errorcode
 
-# for mysql tables
+# for displaying results as mysql tables
 # pip install tabulate
 from tabulate import tabulate
 
@@ -19,35 +21,59 @@ config = {
     "raise_on_warnings": True
 }
 
-# program loop
+# Global variables
 IS_RUNNING = True
 USER_ID = 0
+logo = "WhatABook 📖"
+DB = None
+
+# loader
+def run_spinner():
+    for i in range(2):
+        for cursor in '\\|/-':
+            time.sleep(0.1)
+            # Use '\r' to move cursor back to line beginning
+            # Or use '\b' to erase the last character
+            sys.stdout.write('{}'.format(cursor))
+            # Force Python to write data into terminal.
+            sys.stdout.flush()
+            sys.stdout.write("\r") #carriage return
 
 def get_db():
+    global DB
     try:
         """ try/catch block for handling potential MySQL database errors """ 
-        db = mysql.connector.connect(**config) # connect to the pysports database 
+        DB = mysql.connector.connect(**config) # connect to the pysports database 
         # output the connection status 
         # print("\n Database user [{}] connected to MySQL on host [{}] with database [{}]".format(config["user"], config["host"], config["database"]))
         # Cursor Example
-        cursor = db.cursor()
+        cursor = DB.cursor()
         return cursor
     except mysql.connector.Error as err:
         """ on error code """
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print("  The supplied username or password are invalid")
+            print("The supplied username or password are invalid")
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print("  The specified database does not exist")
+            print("The specified database does not exist")
         else:
             print(err)
     finally:
         print("""\n\n\tclosing connection to MySQL """)
-        db.close()
+        DB.close()
 
+def close_db():
+    global DB
+    try: 
+        DB.close()
+        print("""\n\tclosing connection to MySQL """)
+    except AttributeError as e:
+        print(f"DB was not used, skipping close action.")            
+
+# display wishlist books for user_id
 def show_wishlist():
-    # display wishlist books for user_id
-    db = mysql.connector.connect(**config) 
-    cursor = db.cursor()
+    global DB
+    DB = mysql.connector.connect(**config) 
+    cursor = DB.cursor()
     cursor.execute(f'''SELECT wishlist.user_id, user.first_name, user.last_name, wishlist.book_id, book.book_name
     FROM wishlist
     INNER JOIN user
@@ -55,109 +81,102 @@ def show_wishlist():
     INNER JOIN book
     ON wishlist.book_id = book.book_id
     WHERE user.user_id = {USER_ID}''')
-    
     results = cursor.fetchall()
-
-    print("\n--- DISPLAYING WISHLIST BOOKS --- \n")
+    print(f"\n{logo}: Displaying Wishlist Books \n")
     print(tabulate(results, headers=['user_id', 'first_name', 'last_name', 'book_id', 'book_name'], tablefmt='psql'))
 
+# display books not in users wishlist that they can add
 def show_available_books():
-    # display books not in users wishlist that they can add
-    db = mysql.connector.connect(**config) 
-    cursor = db.cursor()
+    global DB
+    DB = mysql.connector.connect(**config) 
+    cursor = DB.cursor()
     cursor.execute(f'''SELECT book_id, book_name, author, details 
         FROM book WHERE book_id 
         NOT IN (SELECT book_id from wishlist where user_id = {USER_ID});''')
-    
     results = cursor.fetchall()
-
-    print("\n--- DISPLAYING AVAILABLE BOOKS --- \n")
+    print(f"\n{logo}: Available Books \n")
     print(tabulate(results, headers=['book_id', 'book_name', 'author', 'details'], tablefmt='psql'))
-
-    book_id = int(input("\nEnter a book_id to add it to your Wishlist:  "))
-    print(f"Entered book_id [{book_id}]")
-    cursor.execute(f'''SELECT book_id
-        FROM book WHERE book_id 
-        NOT IN (SELECT book_id from wishlist where user_id = {USER_ID});''')
-    book_ids = cursor.fetchall()   
-    print("book_ids", book_ids)
-    valid_entry = None
-    for item in book_ids:
-        print(f"is book_id: [{item[0]}] == {book_id}") 
-        if book_id == item[0]:
-            valid_entry = True
-            print("valid_entry: ", valid_entry)
-            break
-        else:
-            valid_entry = False
-            print("valid_entry: ", valid_entry)
-    try:
-        if valid_entry:
-            cursor = db.cursor()
+    # get book_id from user
+    run_spinner()
+    try: 
+        book_id = int(input(f"\n{logo}: Enter a book_id to add it to your Wishlist: >>> "))
+        print(f"{logo}: Entered book_id [{book_id}]")
+        cursor.execute(f'''SELECT book_id
+            FROM book WHERE book_id 
+            NOT IN (SELECT book_id from wishlist where user_id = {USER_ID});''')
+        book_ids = cursor.fetchall()  
+        # print("book_ids", book_ids) 
+        # print("book_ids", type(book_ids))
+        book_id_list = []
+        for item in book_ids:
+            book_id_list.append(item[0])
+            # print(book_id_list)
+        # validate book selection against book_ids
+        if book_id in book_id_list:
+            # print(f"book_id: {book_id} == {book_id_list}") 
+            is_valid = True
+            print(f"{logo}: valid book entry...✅")
+            cursor = DB.cursor()
             cursor.execute(f'''INSERT INTO 
                     wishlist (user_id, book_id)
                     VALUES ({USER_ID}, {book_id});''')
-            db.commit()
-            print("ADDED book to Wishlist")
+            DB.commit()
+            print(f"{logo}: Added book to Wishlist")
             show_wishlist()
-
         else:
-            print("INVALID book_id")
+            raise Exception("book_id was not found.")    
+          
     except Exception as e:
-        print(f"Exception: {e}")
-        show_wishlist_menu() 
+        print(f"{logo}: Whoa let's try that again: [ {e} ]...👀")
+        show_wishlist_menu()
         
 def remove_book():
+    global DB
     # show wishlist
     show_wishlist()
-
     # get book_id to be removed
-    book_id = int(input("\nEnter book_id to be removed:  "))
-
+    run_spinner()
+    book_id = int(input(f"\n{logo}: Enter book_id to be removed: >>> "))
     # delete (user_id, book_id) from wishlist
-    db = mysql.connector.connect(**config) 
-    cursor = db.cursor()
-    cursor.execute(f'''DELETE FROM wishlist WHERE user_id={USER_ID} AND book_id={book_id};''')
-    db.commit()
-    print(f"DELETED book_id: [{book_id}] from Wishlist...")
-    # show wishlist
+    DB = mysql.connector.connect(**config) 
+    cursor = DB.cursor()
+    cursor.execute(f"DELETE FROM wishlist WHERE user_id={USER_ID} AND book_id={book_id};")
+    DB.commit()
+    print(f"{logo}: Deleted book_id: [{book_id}] from Wishlist...❌")
     show_wishlist()
 
+# display all books
 def show_books():
-    # display all books 
-    db = mysql.connector.connect(**config) # connect to the pysports database 
-    cursor = db.cursor()
+    global DB
+    DB = mysql.connector.connect(**config) # connect to the pysports database 
+    cursor = DB.cursor()
     cursor.execute("SELECT book_id, book_name, details, author FROM book;")
     results = cursor.fetchall()
-    print("\n--- DISPLAYING BOOK RECORDS --- \n")
-    for item in results:
-        print(f"Book ID: {item[0]}")
-        print(f"Book name: {item[1]}")
-        print(f"Detais: {item[2]}")
-        print(f"Author: {item[3]}\n")
+    print(f"\n{logo}: Displaying Book Records \n")
+    print(tabulate(results, headers=['book_id', 'book_name', 'author', 'details'], tablefmt='psql')) 
 
+# display stores
 def show_stores():
-    # display all books 
-    db = mysql.connector.connect(**config) # connect to the pysports database 
-    cursor = db.cursor()
+    global DB
+    DB = mysql.connector.connect(**config) # connect to the pysports database 
+    cursor = DB.cursor()
     cursor.execute("SELECT store_id, locale FROM store;")
     results = cursor.fetchall()
-    print("\n--- DISPLAYING STORE LOCATIONS --- \n")
-    for item in results:
-        print(f"Store ID: {item[0]}")
-        print(f"Locale: {item[1]}")
+    print(f"\n{logo}: Store Locations \n")
+    print(tabulate(results, headers=['store_id', 'locale'], tablefmt='psql'))
 
+# display wishlist menu
 def show_wishlist_menu():
     global USER_ID
-    print("\nWhatABook: Wishlist Menu")
-    print(f"For user_id: [ {USER_ID} ]")
-    print("[ 1 ] Wishlist")
-    print("[ 2 ] Add Book")
-    print("[ 3 ] Remove Book")
-    print("[ x ] Exit to Main")
-    result = input("\nMake a selection:  ")
-    print(f"You selected: [ {result} ]")
-
+    print(f"\n{logo}: [Wishlist Menu]")
+    print(f"{logo}: logged in as 👤 user_id: [ {USER_ID} ]")
+    print(f"{logo}: Press [ 1 ] Wishlist")
+    print(f"{logo}: Press [ 2 ] Add Book")
+    print(f"{logo}: Press [ 3 ] Remove Book")
+    print(f"{logo}: Press [ x ] Exit to Main menu")
+    run_spinner()
+    result = input(f"\n{logo}: Make a selection: >>> ")
+    print(f"{logo}: You selected: [ {result} ]")
     if result == '1':
         show_wishlist()
     elif result == '2':
@@ -167,54 +186,58 @@ def show_wishlist_menu():
     elif result.lower().strip() == 'x':
         USER_ID = 0 
     else:
-        print("\n*** INVALID Selection")
-        print("*** Please try again\n")          
+        print(f"\n{logo}: *** Whoa let's try that again...👀")        
 
+# display main menu
 def show_menu():
-    print("\nWhatABook: Main Menu")
-    print("[ 1 ] View Books")
-    print("[ 2 ] View Store Locations")
-    print("[ 3 ] My Account")
-    print("[ x ] Exit")
     
-    result = input("\nMake a selection:  ")
-    print(f"You selected: [ {result} ]") 
-    
-    if result == '1':
-        show_books()
-    
-    elif result == '2':
-        show_stores()
+    print(f"\n{logo}: Main Menu ")
+    print(f"{logo}: Press [ 1 ] View Books")
+    print(f"{logo}: Press [ 2 ] View Store Locations")
+    print(f"{logo}: Press [ 3 ] My Account")
+    print(f"{logo}: Press [ x ] Exit")
+    run_spinner()
+    result = input(f"\n{logo}: Make a selection: >>> ")
+    try: 
+        print(f"{logo}: You selected: [ {result} ]") 
+        if result == '1':
+            show_books()
+        elif result == '2':
+            show_stores()
+        elif result == '3':
+            print(f"\n{logo}: Which User Account ID?")
+            print(f"{logo}: Press [ 1 ] for Ryan ")
+            print(f"{logo}: Press [ 2 ] for Gabriel ")
+            print(f"{logo}: Press [ 3 ] for Aila ")
+            try:
+                run_spinner()
+                result = int(input(f"\n{logo}: Enter Account ID: >>> ").strip())
+                global USER_ID
+                if result < 4:
+                    USER_ID = result  
+                    print(f"{logo}: Using Account: [ {USER_ID} ]")
+                else:
+                    print(f"\n{logo}: *** Invalid Account ID...👀")
+            except Exception as e:
+                print(f"{logo}: Whoa let's try that again: [ {e} ]...👀")
+                show_menu()        
+        elif result.lower().strip() == 'x':
+            global IS_RUNNING 
+            IS_RUNNING = False
+            print(f"{logo}: See you next time 👋")
+            close_db()
+        else:
+            print(f"{logo}: Input not recognized: [ {result} ]")
+    except Exception as e:
+        print(f"{logo}: Whoa let's try that again: [ {e} ]...👀")
+        pass
 
-    elif result == '3':
-        print("\nWhich user account?")
-        print("[ 1 ] Ryan ")
-        print("[ 2 ] Gabriel ")
-        print("[ 3 ] Aila ")
-        try:
-            result = int(input("\nEnter ID:  ").strip())
-            global USER_ID
-            if result < 4:
-                USER_ID = result  
-                print(f"Using Account: [ {USER_ID} ]")
-            else:
-                print("\n*** INVALID Account ID")
-                print("*** Please try again\n")
-        except Exception as e:
-            print(f"INVALID entry: {e}")
-            show_menu()        
-
-    
-    elif result.lower().strip() == 'x':
-        global IS_RUNNING 
-        IS_RUNNING = False
-    else:
-        print("\n*** INVALID Selection")
-        print("*** Please try again\n")
-
+# Start program loop
 while IS_RUNNING:
     if USER_ID != 0:
+        run_spinner()
         show_wishlist_menu()
     else:
+        run_spinner()
         show_menu()
 
